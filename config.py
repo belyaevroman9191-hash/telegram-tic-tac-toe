d494fe51c4b2504e7c2cf3830f9a30b61dd7c76f
belyaevroman9191-hash <belyaev.roman9191@gmail.com>
2026-07-29 09:55:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,25 +1,24 @@
 import os
 import sqlite3
 
-# Запрещаем Render создавать дублирующие процессы бота при старте
 os.environ["WEB_CONCURRENCY"] = "1"
 
-# Безопасное получение переменных из панели управления Render
 TOKEN = os.getenv("BOT_TOKEN", "8756387431:AAHVVg2yXaHFC_XngwuwAkODLz7yUEQY2XA")
 SERVER_URL = os.getenv("SERVER_URL", "https://telegram-tic-tac-toe-8dv1.onrender.com")
 ADMIN_SECRET = os.getenv("ADMIN_SECRET", "SUPER_SECRET_WINS_KEY_99")
 
-# Настройка и инициализация базы данных
 db_path = os.path.join(os.path.dirname(__file__), "tic_tac_toe.db")
 conn = sqlite3.connect(db_path, check_same_thread=False)
 cursor = conn.cursor()
 
+# ДОБАВЛЕНО ПОЛЕ draws СЮДА
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS users (
     user_id INTEGER PRIMARY KEY,
     username TEXT,
     wins INTEGER DEFAULT 0,
     losses INTEGER DEFAULT 0,
+    draws INTEGER DEFAULT 0,
     current_skin TEXT DEFAULT 'classic',
     unlocked_skins TEXT DEFAULT 'classic'
 )
@@ -30,7 +29,7 @@ SKINS_CONFIG = {
     "classic": {"name": "Классика", "cost": 0, "x": "❌", "o": "⭕"},
     "ninja": {"name": "Дуэль Ниндзя", "cost": 3, "x": "⚔️", "o": "🛡️"},
     "elements": {"name": "Магия стихий", "cost": 5, "x": "🔥", "o": "💧"},
-    "halloween": {"name": "Хэллоуин", "cost": 10, "x": "🎃", "o": "👻"},
+    "halloween": {"name": "Хэллоуин", "cost": 10, "x": "🎃", "o": "💀"},
     "space": {"name": "Космос", "cost": 15, "x": "🚀", "o": "🛸"}
 }
 
