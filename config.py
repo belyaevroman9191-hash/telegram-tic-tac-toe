f222860e13623d0b4bcb2a82a351ee665a7276cd
belyaevroman9191-hash <belyaev.roman9191@gmail.com>
2026-07-29 09:24:18 +0300

    Update config.py

--- config.py
+++ config.py
@@ -28,10 +28,10 @@ conn.commit()
 
 SKINS_CONFIG = {
     "classic": {"name": "Классика", "cost": 0, "x": "❌", "o": "⭕"},
-    "ninja": {"name": "Дуэль Ниндзя", "cost": 3, "x": "⚔️", "o": "Ὦ️"},
-    "elements": {"name": "Магия стихий", "cost": 5, "x": "ὒ", "o": "Ὂ"},
-    "halloween": {"name": "Хэллоуин", "cost": 10, "x": "὇", "o": "Ἰ"},
-    "space": {"name": "Космос", "cost": 15, "x": "Ὠ", "o": "὇"}
+    "ninja": {"name": "Дуэль Ниндзя", "cost": 3, "x": "⚔️", "o": "🥷"},
+    "elements": {"name": "Магия стихий", "cost": 5, "x": "🔥", "o": "💧"},
+    "halloween": {"name": "Хэллоуин", "cost": 10, "x": "🎃", "o": "👻"},
+    "space": {"name": "Космос", "cost": 15, "x": "🚀", "o": "🛸"}
 }
 
 game_rooms = {}
